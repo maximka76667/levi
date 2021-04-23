587220136dd6f153981ca223fbeac2fc8c5376d3
max76667 <max766667@gmail.com>
2021-04-23 22:35:23 +0600

    feat: добавил jquery, плавное перемещение по якорям

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,11 @@
 import '../pages/index.css';
+
+const homeButton = document.querySelector('.home-button');
+
+window.addEventListener('scroll', () => {
+  if (pageYOffset >= 200) {
+    homeButton.classList.add('home-button_active');
+  } else {
+    homeButton.classList.remove('home-button_active');
+  }
+});
