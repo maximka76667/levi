432e1230a53f4c8b76e887d3877cecc2ba566b16
max76667 <max766667@gmail.com>
2021-04-24 12:39:09 +0600

    fix: добавил эффект скролла на ссылку в hero

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -11,4 +11,10 @@ $(document).ready(function () {
       top = $(id).offset().top;
     $('body, html').animate({ scrollTop: top }, 800);
   });
+  $('#landingLink').on('click', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body, html').animate({ scrollTop: top }, 800);
+  });
 });
