71962b9dcc9ae581e459181046110f487f2acdf8
max76667 <max766667@gmail.com>
2021-04-24 12:23:39 +0600

    style: добавил анимацию шкалам скиллов

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import '../pages/index.css';
 
 const homeButton = document.querySelector('.home-button');
 
+const progressBars = document.querySelectorAll('.profile__bar-front');
+const barsSettings = ['100%', '70%', '80%'];
+
 window.addEventListener('scroll', () => {
   if (pageYOffset >= 200) {
     homeButton.classList.add('home-button_active');
@@ -9,3 +12,14 @@ window.addEventListener('scroll', () => {
     homeButton.classList.remove('home-button_active');
   }
 });
+
+window.addEventListener('scroll', activateSkillBars);
+
+function activateSkillBars() {
+  if (pageYOffset >= 800) {
+    for (let i = 0; i <= barsSettings.length - 1; i++) {
+      progressBars[i].style.width = barsSettings[i];
+    }
+    window.removeEventListener('scroll', activateSkillBars);
+  }
+}
